26c49a3e90719104a6d6e2a17abd79725652faa0
kembourne <kembourne93@gmail.com>
2023-07-05 12:01:03 +0500

    env issue updated

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,36 +4,54 @@ import { useState,useEffect } from 'react'
 import { carDetailsInterface } from '.'
 import Image from 'next/image'
 import hero from '../public/hero.png'
-import {getCarImages} from '../components/Request'
-export default async function CarDetails({details}:carDetailsInterface ) {
-  const [images,setImages]=useState();
+import { getCarImages } from './Request'
+interface roughProp {
+  full : string
+  raw : string
+  regular: string
+  small: string
+  small_s3 : string
+  thumb: string
+  
+}
+export default function CarDetails({details}:carDetailsInterface ) {
+  const entries = Object.entries(details);
+  const [image,generateImage]=useState<roughProp>();
   useEffect(()=>{
-    const res = getCarImages("nissan");
-    console.log(res);
-    const entries = Object.entries(details);
+const res =getCarImages("nissan").then(res=>generateImage(res.results[0].urls))
   },[])
+console.log(image?.raw)
   return (
   <div>
+ 
    <Image 
-   src={hero}
+   src={image!== undefined ? image.full :  ""} 
    alt='none'
    className='w-56 md:w-96 bg-blue-700 rounded-[30px] '
+   width={224}
+   height={100}
    />
 <div className='flex flex-row w-full gap-1 justify-evenly'>
 <Image 
    src={hero}
    alt='none'
    className='w-16 md:w-24 bg-blue-700 mt-2 '
+   width={64}
+   height={64}
    />
     <Image 
    src={hero}
    alt='none'
    className='w-16 md:w-24 bg-blue-700 mt-2 '
+   width={64}
+   height={64}
    />
     <Image 
    src={hero}
    alt='none'
    className='w-16 md:w-24 bg-blue-700 mt-2 '
+   width={64}
+   height={64}
    />
 </div>
 
--- components/Request.ts
+++ components/Request.ts
@@ -1,51 +1,40 @@
-import axios from 'axios';
-export const getCars=async (carMake:string,carModal:string)=>{
+import axios from "axios";
+export const getCars = async (carMake: string, carModal: string) => {
   const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=${carModal}`;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '538ade6eefmsh16448dfc43f6e45p1a22abjsn47ddeca3466b',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "538ade6eefmsh16448dfc43f6e45p1a22abjsn47ddeca3466b",
+      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	return result;
-} catch (error) {
-	console.error(error);
-}
-}
+export const calculateRent = (milage: number, year: number) => {
+  if (year > 1990 && year < 2000) return milage * 270;
+  else if (year > 2000 && year < 2010) return milage * 350;
+  else year > 2010;
+  return milage * 500;
+};
 
-export const calculateRent=(milage:number ,year:number)=>{
- if(year>1990 && year<2000)
- return milage*270;
- else if(year>2000 && year<2010)
- return milage*350;
- else(year>2010)
- return milage*500;
-}
+export async function getCarImages(carName: string) {
+  const accessKey = "gvKl3Ey-wjJkO85-JXI4YeeCMrKixffXAliefJyQ4ho";
+  const url = `https://api.unsplash.com/search/photos?query=${carName}&client_id=${accessKey}`;
 
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
 
-
-
-
-  export async function getCarImages(carName:string) {
-    const accessKey = 'gvKl3Ey-wjJkO85-JXI4YeeCMrKixffXAliefJyQ4ho';
-    const url = `https://api.unsplash.com/search/photos?query=${carName}&client_id=${accessKey}`;
-  
-    try {
-      const response = await fetch(url);
-      const result = await response.json();
-      
-      if (result.results.length > 0) {
-      return result;
-      } else {
-        console.log('No results found.');
-      }
-    } catch (error) {
-      console.error('Error:');
-    }
+    return result;
+  } catch (error) {
+    console.error("Error:");
   }
-  
+}
